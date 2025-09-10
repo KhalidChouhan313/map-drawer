16426023233bc5574d7fbbae7e6e14a7ed2bc948
khalid chouhan <khalidchuhan7886@gmail.com>
2025-09-10 22:38:19 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/header/header.component";
 import { SidebarComponent } from "./core/sidebar/sidebar.component";
 import { MapCommponentComponent } from "./feartures/map-commponent/map-commponent.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, MapCommponentComponent],
+  imports: [ HeaderComponent, SidebarComponent, MapCommponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
